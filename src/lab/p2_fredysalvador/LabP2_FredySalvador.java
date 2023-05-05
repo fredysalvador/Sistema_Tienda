e0f1fa67b2ce0b2e7e92833b8e03d608e05f3d90
Fredy Salvador <Fredy Salvador@DESKTOP-N10PBLA>
2023-05-05 13:55:45 -0600

    LaboratorioIII LAB2 - Sistema de Inventario-Formulario

--- src/lab/p2_fredysalvador/LabP2_FredySalvador.java
+++ src/lab/p2_fredysalvador/LabP2_FredySalvador.java
@@ -4,7 +4,9 @@
  */
 package lab.p2_fredysalvador;
 
-/**
+import java.util.Scanner;
+
+/*
  *
  * @author Fredy Salvador
  */
@@ -14,8 +16,54 @@ public class LabP2_FredySalvador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       // TODO code application logic here
+       
+        Scanner leer = new Scanner(System.in);
+       
+        int contador = 0;
+        while (contador != 8){
+            
+        System.out.println("Tienda Fredy Salvador\n1-Agregar Comida\n2-Agregar Bebida\n3-Modificar Productos\n4-Eliminar Producto\n5-Mostrar Producto\n6-Generar Compra\n7-Registro Compras\n8-Salir");
+        contador = leer.nextInt();
         
+        if (contador == 1){
+            
+        System.out.println("");
+            
+        }else if (contador == 2){
+        
+        
+            
+        }else if (contador == 3){
+        
+            
+            
+        }else if (contador == 4){
+        
+            
+            
+        }else if (contador == 5){
+        
+            
+            
+        }else if (contador == 6){
+        
+            
+            
+        }else if (contador == 7){
+        
+            
+            
+        }else if (contador == 8){
+       
+            
+            
+        }
+        
+        
+        
+ 
+        }
         
         
     }
