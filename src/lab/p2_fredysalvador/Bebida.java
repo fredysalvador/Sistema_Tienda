be1e74dbad3a82d935a131de71cb01d56ab2f36c
Fredy Salvador <Fredy Salvador@DESKTOP-N10PBLA>
2023-05-05 13:35:16 -0600

    LaboratorioIII LAB2 - Sistema de Inventario-Constructor-Getter-Setters

--- src/lab/p2_fredysalvador/Bebida.java
+++ src/lab/p2_fredysalvador/Bebida.java
@@ -13,4 +13,36 @@ private String Nombre;
 private Double Precio;
 private Double Tamaño_Ml;
 
+    public Bebida(String Nombre, Double Precio, Double Tamaño_Ml) {
+        this.Nombre = Nombre;
+        this.Precio = Precio;
+        this.Tamaño_Ml = Tamaño_Ml;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public Double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(Double Precio) {
+        this.Precio = Precio;
+    }
+
+    public Double getTamaño_Ml() {
+        return Tamaño_Ml;
+    }
+
+    public void setTamaño_Ml(Double Tamaño_Ml) {
+        this.Tamaño_Ml = Tamaño_Ml;
+    }
+    
+    
+
 }
--- src/lab/p2_fredysalvador/Comida.java
+++ src/lab/p2_fredysalvador/Comida.java
@@ -13,5 +13,37 @@ private String Nombre;
 private Double Precio;
 private boolean Estado_Vencm;
 
+    public Comida(String Nombre, Double Precio, boolean Estado_Vencm) {
+        this.Nombre = Nombre;
+        this.Precio = Precio;
+        this.Estado_Vencm = Estado_Vencm;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public Double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(Double Precio) {
+        this.Precio = Precio;
+    }
+
+    public boolean isEstado_Vencm() {
+        return Estado_Vencm;
+    }
+
+    public void setEstado_Vencm(boolean Estado_Vencm) {
+        this.Estado_Vencm = Estado_Vencm;
+    }
+
+    
+    
 
 }
